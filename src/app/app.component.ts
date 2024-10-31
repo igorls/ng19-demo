e17aaa6797434bfaeb3f4f4ee540394d6c299123
Igor Lins e Silva <4753812+igorls@users.noreply.github.com>
2024-10-31 02:39:31 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, computed, inject, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {JsonPipe} from '@angular/common';
+import {rxResource, toObservable, toSignal} from '@angular/core/rxjs-interop';
+import {debounceTime, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng19-demo';
+  http = inject(HttpClient);
+  getBlockUrl = `https://eos.hyperion.eosrio.io/v1/chain/get_block`;
+  blockNumber = signal<string>('');
+  blockNumber$ = toObservable(this.blockNumber).pipe(debounceTime(300));
+  debouncedBlockNumber = toSignal(this.blockNumber$);
+  blockData = rxResource({
+    request: () => parseInt(this.debouncedBlockNumber() || '1'),
+    loader: ({request: blockNum}) => {
+      return this.http.post(this.getBlockUrl, {block_num_or_id: blockNum}) as Observable<any>;
+    }
+  });
+  errorString = computed<string>(() => {
+    return (this.blockData.error() as any).error.message;
+  });
 }
